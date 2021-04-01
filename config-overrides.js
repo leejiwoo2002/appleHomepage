406e256bb7a1e94f1f44b4152fcb2a73e80763cf
leejiwoo2002 <leejiwoo2002@naver.com>
2021-04-01 12:36:05 +0900

    chore : alias 설정 추가

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+/* eslint-disable no-useless-computed-key */
+const path = require('path');
+const { addWebpackAlias, override } = require('customize-cra');
+
+module.exports = {
+  webpack: override(
+    addWebpackAlias({
+      ['@components']: path.resolve(__dirname, 'src/components/'),
+    })
+  ),
+};
