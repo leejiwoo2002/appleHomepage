c41bf782a824ed74b107e66567804af0162c9518
leejiwoo2002 <leejiwoo2002@naver.com>
2021-04-02 02:27:40 +0900

    feat : 상단 Navbar testcode 구현

--- src/components/header/header.test.ts
+++ src/components/header/header.test.ts
@@ -2,7 +2,17 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../../App';
 
-it('renders welcome message', () => {
+it('Navbar PC render', () => {
   render(React.createElement(App));
-  expect(screen.getByText('iphone')).toBeInTheDocument();
+  expect(screen.getByText('Mac')).toBeInTheDocument();
+  expect(screen.getByText('iPad')).toBeInTheDocument();
+  expect(screen.getByText('iPhone')).toBeInTheDocument();
+  expect(screen.getByText('Watch')).toBeInTheDocument();
+  expect(screen.getByText('Music')).toBeInTheDocument();
+  expect(screen.getByText('고객지원')).toBeInTheDocument();
+  expect(screen.getByText('검색')).toBeInTheDocument();
 });
+
+// todo
+// it('Navbar Mobile render', () => {
+// });
