1980bf4384de6031854cc10c1b9aac033c76c1db
leejiwoo2002 <leejiwoo2002@naver.com>
2021-04-02 02:27:21 +0900

    feat : 상단 Navbar 반응형 레이아웃 적용

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { ReactComponent as AppleLogo } from '@resources/images/appleLogo.svg';
+import { ReactComponent as BagLogo } from '@resources/images/bag.svg';
 
 const Container = styled.div`
   width: 100%;
@@ -13,6 +14,11 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   z:index: 1;
+  min-width: 320px;
+
+  @media (max-width: 800px) {
+    height: 48px;
+  } ;
 `;
 
 const NavBar = styled.div`
@@ -28,16 +34,41 @@ const NavBar = styled.div`
 const NavButton = styled.a`
   font-size: 14px;
   color: #f5f5f7;
+  padding: 0 10px;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  opacity: 0.8;
+  transition-delay: 0s;
+  transition-duration: 0.3s;
+  transition-property: opacity;
+  transition-timing-function: cubic-bezier(0.25, 0.1, 0.25, 1);
   :hover {
     cursor: pointer;
+    opacity: 1;
   }
 `;
 
 function Header(): JSX.Element {
-  return (
-    <Container>
-      <NavBar>
-        <AppleLogo></AppleLogo>
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  function handleResize() {
+    setScreenWidth(window.innerWidth);
+  }
+
+  function pc(): JSX.Element {
+    return (
+      <>
+        <NavButton>
+          <AppleLogo></AppleLogo>
+        </NavButton>
         <NavButton>Mac</NavButton>
         <NavButton>iPad</NavButton>
         <NavButton>iPhone</NavButton>
@@ -45,8 +76,30 @@ function Header(): JSX.Element {
         <NavButton>Music</NavButton>
         <NavButton>고객지원</NavButton>
         <NavButton>검색</NavButton>
-        <NavButton>장바구니</NavButton>
-      </NavBar>
+        <NavButton>
+          <BagLogo></BagLogo>
+        </NavButton>
+      </>
+    );
+  }
+
+  function mobile(): JSX.Element {
+    return (
+      <>
+        <NavButton>목록</NavButton>
+        <NavButton>
+          <AppleLogo></AppleLogo>
+        </NavButton>
+        <NavButton>
+          <BagLogo></BagLogo>
+        </NavButton>
+      </>
+    );
+  }
+
+  return (
+    <Container>
+      <NavBar>{screenWidth > 800 ? pc() : mobile()}</NavBar>
     </Container>
   );
 }
